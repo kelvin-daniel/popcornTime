9bd284421448266d4b182ff544a7ed426ba2dc55
kelvin-daniel <kaymutor@gmail.com>
2020-09-17 14:06:13 +0300

    add login function

--- manage.py
+++ manage.py
@@ -1,5 +1,7 @@
-from app import create_app
+from app import create_app, db
 from flask_script import Manager,Shell,Server
+from app.models import User, Role
+from  flask_migrate import Migrate, MigrateCommand
 
 # Creating app instance
 app = create_app('development')
@@ -8,12 +10,18 @@ manager = Manager(app)
 
 manager.add_command('server',Server)
 
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 @manager.command
 def test():
     """Run the unit tests."""
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(testsg)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User , Role = Role)
 
 if __name__ == '__main__':
     manager.run()
